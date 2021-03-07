479caa190539d7ff2467e287b46ed47140cc0f65
abrorbek <abrorbekgithub@gmail.com>
2021-03-07 11:16:58 +0400

    import flask request

--- request_data.py
+++ request_data.py
@@ -1,21 +1,10 @@
-import requests
-from flask import Flask
-from pprint import pprint
-from tinydb import TinyDB,Query
+from flask import Flask,request
 
 app=Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "hello world!"
-
-@app.route('/index')
-def index():
-    return "index"
-
-@app.route('/home')
-def home():
-    return "home page"
+    return "ok"
 
 if __name__=="__main__":
     app.run(debug=True)
