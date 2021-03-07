f09d3ac7455095fb30a9ac59a09d01bd9f773ad1
abrorbek <abrorbekgithub@gmail.com>
2021-03-07 11:31:14 +0400

    request.methods

--- request_data.py
+++ request_data.py
@@ -4,7 +4,22 @@ app=Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "ok"
+    path = request.path
+    full_path = request.full_path
+    script_root = request.script_root
+    base_url = request.base_url
+    url = request.url
+    url_root = request.url_root
+
+    total=f"""
+        <p>{path}</p>
+        <p>{full_path}</p>
+        <p>{script_root}</p>
+        <p>{base_url}</p>
+        <p>{url}</p>
+        <p>{url_root}</p>
+    """
+    return total
 
 if __name__=="__main__":
     app.run(debug=True)
